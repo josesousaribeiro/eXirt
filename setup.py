3f3685c3b2f2ed90c3434ba87c1f0c3818f4b8d0
José de Sousa Ribeiro Filho <jose.sousa.filho@gmail.com>
2023-01-12 17:15:51 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='eXirt',
+      description='Explainable Artificial Intelligence tool base in Item Response Theory.',
+      author='José de Sousa Ribeiro Filho',
+      author_email='jose.sousa.filho@gmail.com',
+      version='1.0.0.6',
+      py_modules=['eXirt.py'],
+      packages=['eXirt']
+      )
+
